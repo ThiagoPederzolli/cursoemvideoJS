2eca0cc91882b58d38171c1b551cbbbafb604420
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-08-29 23:42:41 -0300

    exercicio 17

--- aula14/aula14ex/ex017/script.js
+++ aula14/aula14ex/ex017/script.js
@@ -1,1 +1,23 @@
-alert('Olá');
+document
+.querySelector('#botao')
+.addEventListener('click', tabuada);
+
+function tabuada(){
+  let num = document.querySelector('#txtn');
+  let tab = document.querySelector('#seltab');
+  if (num.value.length == 0){
+    alert('Por favor, digite um número');
+  }else {
+    let n = Number(num.value);
+    let c = 1;
+    tab.innerHTML = ''
+    while (c <= 10){
+      let item = document.createElement('option');
+      item.text = `${n} x ${c} = ${n*c}`
+      item.value = `tab${c}`
+      tab.appendChild(item);
+      c += 1;
+    }
+
+  }
+}
